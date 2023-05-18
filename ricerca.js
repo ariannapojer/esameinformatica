ecb0c1e6adcd81c0519b6bc91d465c3a7f08add1
ariannapojer <131856912+ariannapojer@users.noreply.github.com>
2023-05-18 19:48:42 +0200

    changes

--- ricerca.js
+++ ricerca.js
@@ -1,4 +1,57 @@
-document.getElementById("search-button").addEventListener("click", function() {
-    window.location.href = "musicatrentina.html";
-  });
+function search() {
+
+  switch ((document.getElementById('searchInput').value).toLowerCase()) {
+      case 'valsugana':
+          location.href = 'musicavalsugana.html';
+          break;
+
+      case 'trento':
+          location.href = 'musicatrento.html';
+          break;
+
+      case 'val di cembra':
+          location.href = 'musicavaldicembra.html';
+          break;
+      
+          case 'val di non':
+            location.href = 'musicavaldinon.html';
+            break; 
+
+      case 'val di sole':
+          location.href = 'musicavaldisole.html';
+           break;
+          
+      case 'alpecimbra':
+           location.href = 'musicaalpecimbra.html';
+           break;    
+      
+      case 'fiemme':
+           location.href = 'musicafiemme.html';
+           break;
+ 
+       case 'val di fiemme':
+           location.href = 'musicafiemme.html';
+           break;
+ 
+       case 'fassa':
+           location.href = 'musicafassa.html';
+           break; 
+           case 'val di sole':
+            location.href = 'musicavaldisole.html';
+             break;
+            
+        case 'val di fassa':
+             location.href = 'musicafassa.html';
+             break;    
+        
+        case 'cembra':
+             location.href = 'musicavaldicembra.html';
+             break;   
+    
+default:
+  location.href = "musicatrentina.html"
+  }
+}
+
+
   
